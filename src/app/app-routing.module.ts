456f14724b26e0c66b2d143263e86c0c9c1559da
unknown <lankadealwis97@gmail.com>
2019-08-08 20:53:23 +0530

    upload

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,42 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+import { P404Component } from '../app/views/error/404.component';
+import { P500Component } from './views/error/500.component';
+import { UploadComponent } from './views/upload/upload.component';
+import { ReportComponent } from './views/report/report.component';
+const routes: Routes = [
+  {
+    path: '404',
+    component: P404Component,
+    data: {
+      title: 'Page 404'
+    }
+  },
+  {
+    path: '500',
+    component: P500Component,
+    data: {
+      title: 'Page 404'
+    }
+  },
+  {
+    path: '',
+    redirectTo: 'upload',
+    pathMatch: 'full',
+  },
+  {
+    path: 'upload',
+    component: UploadComponent
+  },
+  {
+    path: 'report',
+    component: ReportComponent
+  }
+
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
---
+++ src/app/app.module.ts
@@ -0,0 +1,33 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import {MatIconModule} from '@angular/material/icon';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { MDBBootstrapModule  } from 'angular-bootstrap-md';
+import { UploadComponent } from './views/upload/upload.component';
+import { ReportComponent } from './views/report/report.component';
+import { P404Component } from './views/error/404.component';
+import { P500Component } from './views/error/500.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    UploadComponent,
+    ReportComponent,
+    P404Component,
+    P500Component
+  ],
+  imports: [
+    BrowserModule,
+    MatIconModule,
+    FontAwesomeModule,
+    MDBBootstrapModule.forRoot(),
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
